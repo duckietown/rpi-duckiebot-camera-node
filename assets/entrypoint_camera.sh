fccb1be3e6c134fa685dc17b8ceed32f87453a56
Andrea Censi <AndreaCensi@users.noreply.github.com>
2018-09-06 20:03:56 +0200

    Update entrypoint_camera.sh

--- assets/entrypoint_camera.sh
+++ assets/entrypoint_camera.sh
@@ -8,11 +8,27 @@ source /home/software/catkin_ws/devel/setup.bash
 # launch camera node
 
 echo launching camera node
-echo Configuration:
+echo Configuration sanity check:
+echo
 echo ROS_MASTER_URI=$ROS_MASTER_URI
-echo ROS_IP=${ROS_IP}
-echo VEHICLE_NAME=$VEHICLE_NAME
-echo DUCKIEFLEET_ROOT=$DUCKIEFLEET_ROOT
-echo HOSTNAME=$HOSTNAME
+echo '^ the above should have either an ip or a fully-formed name that resolves to an IP'
+echo '  it is wrong if it is "duckiebot:11311" - it should be "duckiebot.local:11311"'
+echo
+echo ROS_HOSTNAME=$ROS_HOSTNAME
+echo '^ same thing, this should be a fully-formed name (duckiebot.local) rather than "duckiebot"'
 
+echo ROS_IP=${ROS_IP}
+echo '^ this might be unset but better that it is set'
+
+echo VEHICLE_NAME=$VEHICLE_NAME
+echo '^ this should be the naked hostname - "duckiebot" and NOT "duckiebot.local"
+echo
+echo DUCKIEFLEET_ROOT=$DUCKIEFLEET_ROOT
+echo
+echo HOSTNAME=$HOSTNAME
+echo '^ on linux systems this is just "duckiebot", does not include ".local"
+echo 'it is a private name, in the sense that it is not resolvable by external computers'
+echo
+echo "Finally, launching:"
+echo roslaunch --wait duckietown camera.launch veh:=${VEHICLE_NAME}
 roslaunch --wait duckietown camera.launch veh:=${VEHICLE_NAME}
