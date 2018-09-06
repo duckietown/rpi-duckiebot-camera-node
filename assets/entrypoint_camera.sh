f3c8623775382bc080bc816fa120971023a388cc
Andrea Censi <AndreaCensi@users.noreply.github.com>
2018-09-06 19:50:10 +0200

    Update entrypoint_camera.sh

--- assets/entrypoint_camera.sh
+++ assets/entrypoint_camera.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+
 # source docker env and catkin_ws
 source /home/software/docker/env.sh
 source /home/software/catkin_ws/devel/setup.bash
 
 # launch camera node
-roslaunch --wait duckietown camera.launch veh:=${HOSTNAME}
+
+echo launching camera node
+echo Configuration:
+echo ROS_MASTER_URI=$ROS_MASTER_URI
+echo ROS_IP=${ROS_IP}
+echo VEHICLE_NAME=$VEHICLE_NAME
+echo DUCKIEFLEET_ROOT=$DUCKIEFLEET_ROOT
+echo HOSTNAME=$HOSTNAME
+
+roslaunch --wait duckietown camera.launch veh:=${VEHICLE_NAME}
