34ea57bb9cb2fdc87025d0aa1604fa6423b2fd52
Andrea Censi <acensi@ethz.ch>
2018-09-07 11:18:40 +0200

    done

--- assets/entrypoint_camera.sh
+++ assets/entrypoint_camera.sh
@@ -7,27 +7,28 @@ source /home/software/catkin_ws/devel/setup.bash
 
 # launch camera node
 
-echo launching camera node
+echo
 echo Configuration sanity check:
-echo HOSTNAME=$HOSTNAME
-echo '^ on linux systems this is just "duckiebot", does not include ".local"
-echo 'it is a private name, in the sense that it is not resolvable by external computers'
+echo
+echo HOSTNAME=${HOSTNAME:-NOT-SET}
+echo "^ on linux systems this is just \"duckiebot\", does not include \".local\""
+echo "it is a private name, in the sense that it is not resolvable by external computers"
 echo
 echo
-echo ROS_MASTER_URI=$ROS_MASTER_URI
-echo '^ the above should have either an ip or a fully-formed name that resolves to an IP'
-echo '  it is wrong if it is "duckiebot:11311" - it should be "duckiebot.local:11311"'
+echo ROS_MASTER_URI=${ROS_MASTER_URI:-NOT-SET}
+echo "^ the above should have either an ip or a fully-formed name that resolves to an IP"
+echo "  it is wrong if it is \"duckiebot:11311\" - it should be \"duckiebot.local:11311\""
 echo
-echo ROS_HOSTNAME=$ROS_HOSTNAME
-echo '^ same thing, this should be a fully-formed name (duckiebot.local) rather than "duckiebot"'
+echo ROS_HOSTNAME=${ROS_HOSTNAME:-NOT-SET}
+echo "^ same thing, this should be a fully-formed name (duckiebot.local) rather than duckiebot"
 echo
-echo ROS_IP=${ROS_IP}
-echo '^ this might be unset but better that it is set'
+echo ROS_IP=${ROS_IP:-NOT-SET}
+echo "^ this might be unset but better that it is set"
 echo
-echo VEHICLE_NAME=$VEHICLE_NAME
-echo '^ this should be the naked hostname - "duckiebot" and NOT "duckiebot.local"
+echo VEHICLE_NAME=${VEHICLE_NAME:-NOT-SET}
+echo "^ this should be the naked hostname - \"duckiebot\" and NOT \"duckiebot.local\""
 echo
-echo DUCKIEFLEET_ROOT=$DUCKIEFLEET_ROOT
+echo DUCKIEFLEET_ROOT=${DUCKIEFLEET_ROOT:-NOT-SET}
 echo
 
 echo "Finally, launching:"
